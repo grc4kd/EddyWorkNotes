d86e78aac9c7acd72e9dc21bbb6719c4b7059b7a
Your Name <you@example.com>
2025-09-19 08:27:52 -0500

    fix: remove unused classes and test files

--- app/PerformanceTest.cs
+++
@@ -1,9 +0,0 @@
-namespace Eddy;
-
-public record PerformanceTest(float Value, string MetricType)
-{
-    public override string ToString()
-    {
-        return string.Format("Metric {0}: value -> {{{1}}}", MetricType, Value);
-    }
-}
