9c70e42f613e682388f7a7f03194e9ba431a32d5
Your Name <you@example.com>
2025-09-22 15:22:18 -0500

    fix: Correct test to ensure event is not triggered when timer is cancelled

--- test/TaskTimerTest.cs
+++ test/TaskTimerTest.cs
@@ -1,40 +1,6 @@
 using Eddy;
 namespace test;
 
-public class TestPeriodicTimer(TimeSpan period) : ITaskTimer
-{
-    public TimeSpan Period { get; } = period;
-    public bool IsRunning { get; private set; } = false;
-
-    public event EventHandler? TimerCompleted;
-    public event EventHandler? TimerPaused;
-    public event EventHandler? TimerResumed;
-
-    public Task StartAsync()
-    {
-        IsRunning = true;
-        return Task.CompletedTask;
-    }
-
-    public async ValueTask<bool> WaitForNextTickAsync(CancellationToken cancellationToken = default)
-    {
-        IsRunning = true;
-
-        // Immediately complete the timer for testing purposes
-        return await ValueTask.FromResult(true);
-    }
-
-    void ITaskTimer.Pause()
-    {
-        throw new NotImplementedException();
-    }
-
-    Task ITaskTimer.ResumeAsync()
-    {
-        throw new NotImplementedException();
-    }
-}
-
 public class TaskTimerTest
 {
     [Fact]
@@ -109,10 +75,10 @@ public class TaskTimerTest
     }
 
     [Fact]
-    public async Task OnTimerCompleted_ShouldTriggerEventWhenTimerIsCancelled()
+    public async Task OnTimerCompleted_ShouldNotTriggerEventWhenTimerIsCancelled()
     {
         // Given
-        TaskTimer timer = new(PeriodTimeSpan: TimeSpan.FromMinutes(1));
+        var timer = new TestPeriodicTimer(TimeSpan.FromMinutes(1));
 
         bool timerCompleted = false;
 
@@ -123,12 +89,9 @@ public class TaskTimerTest
         try
         {
             // When
-            var task = Task.Run(timer.StartAsync);
             await timer.CancelAsync();
-            await task;
 
             // Then
-            Assert.False(task.IsCanceled);
             Assert.False(timerCompleted);
         }
         finally
