491fd84008b9780c3b6f3181d68086e6db5e6f52
Your Name <you@example.com>
2025-09-30 01:29:46 -0500

    chore: moved default times into record class for task timer settings

--- ui/Components/Models/TaskTimerSettings.cs
+++ ui/Components/Models/TaskTimerSettings.cs
@@ -2,6 +2,8 @@ namespace ui.Components.Models;
 
 public class TaskTimerSettings(TimeSpan WorkTimeSpan, TimeSpan BreakTimeSpan)
 {
+    public TaskTimerSettings() : this(TimeSpan.FromMinutes(25), TimeSpan.FromMinutes(5)) { }
+
     public TimeSpan WorkTimeSpan { get; } = WorkTimeSpan;
     public TimeSpan BreakTimeSpan { get; } = BreakTimeSpan;
 }
