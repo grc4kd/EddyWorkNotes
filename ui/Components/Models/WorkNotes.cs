a4adf5a4ee5e1a00e8cab8f0d33f1476f97ec135
Your Name <you@example.com>
2025-10-08 10:51:17 -0500

    feat: add new, hard-coded example report view with QuickGrid component

--- ui/Components/Models/WorkNotes.cs
+++ ui/Components/Models/WorkNotes.cs
@@ -2,13 +2,13 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ui.Components.Models;
 
-public record WorkNotes : IWorkNotes
+public record WorkNote : IWorkNotes
 {
     private const int MaxWorkNoteDescription = 1000;
     // must be constant for StringLength validator attribute
     private const string ValidationErrorMessage = $"Work notes description is too long (1000 character limit).";
 
-    public WorkNotes(string description = "")
+    public WorkNote(string description = "")
     {
         // truncate description input to maximum length in data layer
         int strlen = description.Length <= MaxWorkNoteDescription ? description.Length : MaxWorkNoteDescription;
