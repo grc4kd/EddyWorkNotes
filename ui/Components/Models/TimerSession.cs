86fdc9bc7f7d63963da4c41925b7547f2ca9c32a
Your Name <you@example.com>
2025-10-13 00:08:30 -0500

    fix: recreate database with UTC timestamps, local formatting on frontend

--- ui/Components/Models/TimerSession.cs
+++ ui/Components/Models/TimerSession.cs
@@ -4,5 +4,6 @@ public record TimerSession(string Message, DateTime CompletedAtUtc, string? Work
 {
     public TimerSession(DataEntities.WorkNote workNote) : this("Work", workNote.RecordedAtTimeUtc.ToUniversalTime(), workNote.Description) { }
 
+    private DateTime _completedAtUtc = CompletedAtUtc;
     public DateTime CompletedAtLocaltime = CompletedAtUtc.ToLocalTime();
 }
