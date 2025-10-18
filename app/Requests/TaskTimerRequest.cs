faaf38e853d0ef56be7e6ae9ea57e3f3f57077b8
Your Name <you@example.com>
2025-10-18 09:25:13 -0500

    feat: Added TaskTimerRequest request DTO class.
    - Added unit test on initially constructed record state.

--- app/Requests/TaskTimerRequest.cs
+++ app/Requests/TaskTimerRequest.cs
@@ -1,6 +1,6 @@
 namespace Eddy.Requests;
 
-public record TaskTimerRequest(TimeSpan Duration)
+public record TaskTimerRequest(TimeSpan WorkTimeSpan, TimeSpan BreakTimeSpan)
 {
-    
+    public readonly TimeSpan TotalDuration = WorkTimeSpan + BreakTimeSpan;
 }
--- test/TaskTimerRequestTests.cs
+++ test/TaskTimerRequestTests.cs
@@ -8,12 +8,15 @@ public class TaskTimerRequestTests
     public void Initialize_WithValidValues_ShouldSetProperties()
     {
         // Given
-        var duration = TimeSpan.FromMinutes(25);
+        var workDuration = TimeSpan.FromMinutes(25);
+        var breakDuration = TimeSpan.FromMinutes(5);
 
         // Act
-        var request = new TaskTimerRequest(duration);
+        var request = new TaskTimerRequest(workDuration, breakDuration);
 
         // Assert
-        Assert.Equal(duration, request.Duration);
+        Assert.Equal(workDuration, request.WorkTimeSpan);
+        Assert.Equal(breakDuration, request.BreakTimeSpan);
+        Assert.Equal(workDuration + breakDuration, request.TotalDuration);
     }
 }
