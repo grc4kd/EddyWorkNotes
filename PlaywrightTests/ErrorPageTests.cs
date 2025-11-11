1ea7e9e64c2a15f2380c81aecdb76c7d2930e89a
Griffin Crane <166270386+grc4kd@users.noreply.github.com>
2025-11-11 12:25:02 -0600

    fix: use test class variable for url

--- PlaywrightTests/ErrorPageTests.cs
+++ PlaywrightTests/ErrorPageTests.cs
@@ -8,7 +8,7 @@ public partial class ErrorPageTests : PageTest
 {
     // test class variables shared across all tests
     const string port = "5085";
-    static readonly Uri url = new($"http://localhost:{port}/tasktimer");
+    static readonly Uri url = new($"http://localhost:{port}/Error");
     static readonly TimeSpan clickDelayTime = TimeSpan.FromMilliseconds(100);
 
     public ErrorPageTests()
@@ -24,7 +24,7 @@ public partial class ErrorPageTests : PageTest
     [TestMethod]
     public async Task ErrorPage_Should_ShowRequestId_WhenAvailable()
     {
-        await Page.GotoAsync("http://localhost:5085/Error");
+        await Page.GotoAsync(url.AbsoluteUri);
 
         await Expect(Page.Locator("h1").First).ToHaveTextAsync("Error.");
         await Expect(Page.Locator("h2").First).ToContainTextAsync("An error occurred");
@@ -37,7 +37,7 @@ public partial class ErrorPageTests : PageTest
     [TestMethod]
     public async Task ErrorPage_Should_DisplayDevelopmentModeInstructions()
     {
-        await Page.GotoAsync("http://localhost:5085/Error");
+        await Page.GotoAsync(url.AbsoluteUri);
 
         await Expect(Page.Locator("body > div.page > main > article > p:nth-child(5)"))
             .ToContainTextAsync("Swapping to Development environment will display more detailed information about the error that occurred.");
@@ -46,7 +46,7 @@ public partial class ErrorPageTests : PageTest
     [TestMethod]
     public async Task ErrorPage_Should_HaveCorrectTitle()
     {
-        await Page.GotoAsync("http://localhost:5085/Error");
+        await Page.GotoAsync(url.AbsoluteUri);
 
         var title = await Page.TitleAsync();
         Assert.AreEqual("Error", title);
@@ -58,7 +58,7 @@ public partial class ErrorPageTests : PageTest
         var consoleMessages = new List<IConsoleMessage>();
         Page.Console += (_, e) => consoleMessages.Add(e);
 
-        await Page.GotoAsync("http://localhost:5085/Error");
+        await Page.GotoAsync(url.AbsoluteUri);
 
         Assert.AreEqual(0, consoleMessages.Where(m => m.Type == "error").Count());
     }
